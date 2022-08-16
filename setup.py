d47cacdfc6a7318c8b97cf7980d605348a86b343
spapa013 <stelios@spapa.us>
2022-08-16 13:27:40 -0500

    update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from os import path
 
 here = path.abspath(path.dirname(__file__))
 
-with open(path.join(here, 'microns_dashboard', 'version.py')) as f:
+with open(path.join(here, 'microns_dashboard_api', 'version.py')) as f:
     exec(f.read())
 
 with open(path.join(here, 'requirements.txt')) as f:
     requirements = f.read().split()
 
 setup(
-    name='microns-utils',
+    name='microns-dashboard',
     version=__version__,
     description='Schemas and notebooks for the MICrONS Jupyterhub Dashboard',
     author='Stelios Papadopoulos, Christos Papadopoulos',
