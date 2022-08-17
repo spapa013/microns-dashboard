a1fb1098d22e782499e7d57b803c48c1b3f0ead8
spapa013 <stelios@spapa.us>
2022-08-17 04:17:56 -0500

    add DataJointLogin app

--- microns_dashboard_api/apps/__init__.py
+++ microns_dashboard_api/apps/__init__.py
@@ -1,7 +1,11 @@
+import json
+import time
+
 import wridgets.app as wra
 from ipywidgets import link
-import json
-from ..utils import get_user_info_js, DashboardUser
+import datajoint_plus as djp
+from ..utils import DashboardUser, get_user_info_js
+
 
 class UserApp(wra.App):
     store_config = [
@@ -44,3 +48,65 @@ class UserApp(wra.App):
     
     def _on_user_update(self):
         pass
+
+
+class DataJointLogin(wra.App):
+    store_config = [
+        'is_connected'
+    ]
+    
+    def make(self, **kwargs):
+        self.hide_on_login = kwargs.get('hide_on_login')
+        self.disable_on_login = kwargs.get('disable_on_login')
+        self.on_login = self.on_login if kwargs.get('on_login') is None else kwargs.get('on_login')
+        self.on_login_kwargs = {} if kwargs.get('on_login_kwargs') is None else kwargs.get('on_login_kwargs')
+        self._header = wra.Label(text='DataJoint', fontsize=2)
+        self._username_label = wra.Label(text='Username', name='UsernameLabel')
+        self._username_field = wra.Field(name='UsernameField')
+        self._password_label = wra.Label(text='Password', name='PasswordLabel')
+        self._password_field = wra.Field(wridget_type='Password', name='PasswordField')
+        self._login_button = wra.Button(description='Login', button_style='info', name='LoginButton', layout={'width': 'initial'}, on_interact=self._on_login) 
+        
+        self.core = (
+            (self._header + self._login_button) - \
+            ((
+                self._username_label - self._password_label
+            ) + \
+            (
+                self._username_field - self._password_field
+            )
+            )
+        )
+        
+    def check_connection(self):
+        try:
+            djp.conn.connection
+            self.is_connected = True
+            self.msg('Connection established.')
+            time.sleep(2)
+        except:
+            self.is_connected = False
+            self.msg('Connection not established.')
+            time.sleep(2)
+    
+    def on_login(self, **kwargs):
+        pass
+    
+    def _on_login(self):
+        if self._username_field.get1('value') != '':
+            import logging
+            logging.disable(50)
+            djp.config['database.user'] = self._username_field.get1('value')
+            djp.config['database.password'] = self._password_field.get1('value')
+            logging.disable(logging.NOTSET)
+            djp.conn(reset=True)
+            self.check_connection()
+            self.clear_output()
+
+            if self.hide_on_login:
+                self.set(layout={'display': 'none'})
+
+            if self.disable_on_login:
+                self.set(disabled=True)
+            
+            self.on_login(**self.on_login_kwargs)
