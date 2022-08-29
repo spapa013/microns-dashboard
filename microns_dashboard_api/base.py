b49aff8256711d0eab5a14c11dd13a1aa053c6f5
spapa013 <stelios@spapa.us>
2022-08-29 13:39:14 -0500

    add features to base

--- microns_dashboard_api/base.py
+++ microns_dashboard_api/base.py
@@ -10,58 +10,84 @@ from time import timezone
 import datajoint as dj
 import datajoint_plus as djp
 from datajoint_plus.utils import format_rows_to_df
-from datajoint_plus.base import BaseMaster, BasePart
-from datajoint_plus.user_tables import UserTable
+from datajoint_plus import base as djpb
+from datajoint_plus import user_tables as djpu
 from microns_utils.misc_utils import classproperty, wrap
 from microns_utils.version_utils import check_package_version
 from microns_utils.datetime_utils import current_timestamp
 
 
-class VersionLookup(BaseMaster, UserTable, dj.Lookup):
+Version = namedtuple('Version', ['id', 'version', 'timestamp'])
+EventData = namedtuple('EventData', ['id', 'name', 'timestamp'])
+
+version_sqltype = "varchar(128)"
+version_id_sqltype = "varchar(6)"
+event_id_sqltype = "varchar(12)"
+event_sqltype = "varchar(128)"
+event_handler_id_sqltype = "varchar(6)"
+
+class Base:
+    @classproperty
+    def definition(cls):
+        return "\n".join([l.strip() for l in [cls.default_primary_attrs, cls.extra_primary_attrs, """---""", cls.default_secondary_attrs, cls.extra_secondary_attrs]]) 
+    
+
+class VersionLookup(Base, djpb.BaseMaster, djpu.UserTable, dj.Lookup):
     def __init_subclass__(cls, **kwargs):
         cls._init_validation(**kwargs)
         assert hasattr(cls, 'package'), 'subclasses of VersionLookup must implement "package"'
+    
     enable_hashing = True
-    hash_name = 'version_id'
-    hashed_attrs = 'version'
+    attr_name = 'version'
+
     @classproperty
-    def definition(cls):
-        return f"""
-                {cls.hash_name} : varchar(6) # hash of package version
-                ---
-                version : varchar(48) # package version
-                timestamp=CURRENT_TIMESTAMP : timestamp # timestamp when version was inserted
-                """
+    def hash_name(cls):
+        return '_'.join([cls.attr_name, 'id'])
+
+    @classproperty
+    def hashed_attrs(cls):
+        return cls.attr_name
     
     @classproperty
-    def current_version(cls):
+    def default_primary_attrs(cls):
+        return f"""
+        {cls.attr_name} : {version_sqltype} # package version
+        """
+    
+    @classproperty
+    def extra_primary_attrs(cls):
+        return """"""
+    
+    @classproperty
+    def default_secondary_attrs(cls):
+        return f"""
+        {cls.hash_name} : {version_id_sqltype} # hash of package version
+        timestamp=CURRENT_TIMESTAMP : timestamp # timestamp when version was inserted
+        """
+
+    @classproperty
+    def extra_secondary_attrs(cls):
+        return """"""
+    
+    @classproperty
+    def version(cls):
         return check_package_version(package=cls.package)
 
     @classproperty
-    def current_version_id(cls):
-        return cls.hash1({'version': cls.current_version})
+    def version_id(cls):
+        return cls.hash1({cls.attr_name: cls.version})
 
     @classproperty
     def contents(cls):
-        cls.insert1({
-            'version' : cls.current_version
-        }, ignore_extra_fields=True, skip_duplicates=True)
+        cls.insert1({cls.attr_name: cls.version}, ignore_extra_fields=True, skip_duplicates=True)
         return {}
 
 
-Event = namedtuple('Event', ['id', 'type', 'timestamp', 'timezone'])
-event_id_sqltype = "varchar(12)"
-event_type_sqltype = "varchar(450)"
-event_ts_sqltype = 'timestamp'
-
-
-class EventLookup(BaseMaster, UserTable, dj.Lookup):
+class EventLookup(Base, djpb.BaseMaster, djpu.UserTable, dj.Lookup):
     def __init_subclass__(cls, **kwargs):
         cls._init_validation(**kwargs)
 
-    enable_hashing = True
     hash_name = 'event_id'
-    hashed_attrs = 'event_type', 'event_ts'
 
     @classmethod
     def get(cls, key):
@@ -73,50 +99,50 @@ class EventLookup(BaseMaster, UserTable, dj.Lookup):
 
     @classproperty
     def default_primary_attrs(cls):
-        return f"""event_id : {event_id_sqltype} \n"""
+        return f"""
+        {cls.hash_name} : {event_id_sqltype}
+        """
 
     @classproperty
-    def additional_primary_attrs(cls):
-        return """\n"""
+    def extra_primary_attrs(cls):
+        return """"""
 
     @classproperty
     def default_secondary_attrs(cls):
         return f"""
-        event_type : {event_type_sqltype}
-        event_ts : {event_ts_sqltype} # event timestamp
+        event=NULL : {event_sqltype}
+        timestamp : timestamp # 
         """
 
     @classproperty
-    def additional_secondary_attrs(cls):
-        return """\n"""
-
-    @classproperty
-    def definition(cls):
-        return cls.default_primary_attrs + cls.additional_primary_attrs + """--- \n""" + cls.default_secondary_attrs + cls.additional_secondary_attrs  
+    def extra_secondary_attrs(cls):
+        return """"""
 
     @classmethod
-    def log_event(cls, event_type, attrs=None, data=None):
-        parts = [part for part in cls.parts(as_cls=True) if event_type in getattr(part, 'event_types')]
-        assert len(parts) >= 1, f'No parts with event_type "{event_type}" found.'
-        assert len(parts) < 2, f'Multiple parts with event_type "{event_type}" found. Parts are: {[p.class_name for p in parts]}'
-        return parts[0].log_event(event_type, attrs=attrs, data=data)
+    def log_event(cls, event, attrs=None, data=None):
+        parts = [part for part in cls.parts(as_cls=True) if event in getattr(part, 'events')]
+        assert len(parts) >= 1, f'No parts with event "{event}" found.'
+        assert len(parts) < 2, f'Multiple parts with event "{event}" found. Parts are: {[p.class_name for p in parts]}'
+        return parts[0].log_event(event, attrs=attrs, data=data)
 
     @classmethod
-    def event_types(cls):
-        event_types = []
+    def events(cls):
+        events = []
         for part in cls.parts(as_cls=True):
-            if issubclass(part, EventType):
-                event_types.extend(wrap(part.event_types))
-        return event_types
+            if issubclass(part, Event):
+                events.extend(wrap(part.events))
+        return events
 
 
-class EventType(BasePart, UserTable, dj.Part):
+class Event(Base, djpb.BasePart, djpu.UserTable, dj.Part):
     def __init_subclass__(cls, **kwargs):
         cls._init_validation(**kwargs)
-        assert getattr(cls, 'event_types', None) is not None, 'Subclasses of EventType must implement "event_types".'
+        assert getattr(cls, 'events', None) is not None, 'Subclasses of Event must implement "events".'
 
-    event_types = None
+    enable_hashing = True
     hash_name = 'event_id'
+    hashed_attrs = 'event', 'timestamp'
+    events = None
     data_type = 'longblob'
     required_keys = None
     external_type = None
@@ -128,33 +154,28 @@ class EventType(BasePart, UserTable, dj.Part):
     def default_primary_attrs(cls):
         return f"""
         -> master
-        event_type : {event_type_sqltype}
-        event_ts : {event_ts_sqltype} # event timestamp
+        event : {event_sqltype}
+        timestamp : timestamp # event timestamp
         """
 
     @classproperty
-    def additional_primary_attrs(cls):
-        return """
-        """
+    def extra_primary_attrs(cls):
+        return """"""
 
     @classproperty
     def default_secondary_attrs(cls):
-        return f"""data=NULL : {cls.data_type} # event associated data. default=NULL \n"""
+        return f"""data=NULL : {cls.data_type} # event associated data. default=NULL"""
 
     @classproperty
-    def additional_secondary_attrs(cls):
-        return """\n"""
+    def extra_secondary_attrs(cls):
+        return """"""
 
-    @classproperty
-    def definition(cls):
-        return cls.default_primary_attrs + cls.additional_primary_attrs + """--- \n""" + cls.additional_secondary_attrs + cls.default_secondary_attrs  
-      
     @classmethod
     def prepare_data(cls, event, data=None):        
         if cls.external_type is not None:
             if cls.external_type == 'filepath':
                 required = ['basedir', 'file_type']
-                assert getattr(cls, required) is not None, f'Subclasses of EventType must implement "{required}" if external_type == "filepath".'
+                assert getattr(cls, required) is not None, f'Subclasses of Event must implement "{required}" if external_type == "filepath".'
                 basedir = Path(cls.basedir)
                 basedir.mkdir(exist_ok=True)
                 filename = basedir.joinpath(event.id).with_suffix(cls.file_type)
@@ -174,18 +195,17 @@ class EventType(BasePart, UserTable, dj.Part):
             return data
     
     @classmethod
-    def log_event(cls, event_type, attrs=None, data=None):
-        assert event_type in cls.event_types, f'event_type not in allowed event_types. event_types: {cls.event_types}'
-        tz = 'US/Central'
-        timestamp = current_timestamp(tz, fmt="%Y-%m-%d_%H:%M:%S.%f")
-        event_id = cls.master.hash1({'event_type': event_type, 'event_ts': timestamp})
-        event = Event(id=event_id, type=event_type, timestamp=timestamp, timezone=tz)
-        row = {'event_id': event.id, 'event_type': event.type, 'event_ts': event.timestamp}
+    def log_event(cls, event, attrs=None, data=None):
+        assert event in cls.events, f'event not found. events: {cls.events}'
+        timestamp = current_timestamp('US/Central', fmt="%Y-%m-%d_%H:%M:%S.%f")
+        event_id = cls.hash1({'event': event, 'timestamp': timestamp})
+        event = EventData(id=event_id, name=event, timestamp=timestamp)
+        row = {'event_id': event.id, 'event': event.name, 'timestamp': event.timestamp}
         row.update({} if attrs is None else attrs)
         row['data'] = cls().prepare_data(event=event, data=data)
-        cls.master.insert1(row=row, constant_attrs={} if cls.constant_attrs is None else cls.constant_attrs, insert_to_parts=cls, ignore_extra_fields=True, skip_hashing=True)
-        cls.master.Log('info', f'Event with event_id {event.id} and event_type "{event.type}" occured at {event.timestamp} {event.timezone}')
-        cls.master.Log('debug', f'Event with event_id {event.id} and event_type "{event.type}" occured at {event.timestamp} {event.timezone} with insert {row}')
+        cls.insert1(row=row, constant_attrs={} if cls.constant_attrs is None else cls.constant_attrs, insert_to_master=True, ignore_extra_fields=True, skip_hashing=True)
+        cls.master.Log('info',  f'Event "{event.name}" with event_id {event.id} occured at {event.timestamp}')
+        cls.master.Log('debug', f'Event "{event.name}" with event_id {event.id} occured at {event.timestamp} with insert {row}')
         cls().on_event(event=event)
         return event
 
@@ -193,36 +213,102 @@ class EventType(BasePart, UserTable, dj.Part):
     def on_event(cls, event):
         pass
 
-    def drop(self, force=False):
-        if force:
-            super(UserTable, self).drop()
-        else:
-            raise dj.DataJointError('Cannot drop a Part directly.  Delete from master instead')
 
-    def drop_quick(self, force=False):
-        if force:
-            return super(UserTable, self).drop_quick()
-        else:
-            raise dj.DataJointError('Cannot drop a Part directly.  Delete from master instead')
-
-
-class EventHandlerLookup(BaseMaster, UserTable, dj.Lookup):
+class EventHandlerLookup(Base, djpb.BaseMaster, djpu.UserTable, dj.Lookup):
     def __init_subclass__(cls, **kwargs):
         cls._init_validation(**kwargs)
 
     hash_name = 'event_handler_id'
-    definition = """
-    event_handler_id : varchar(6) # id of event handler
-    """
-    current_version_id = ''
 
-    @classmethod
-    def run(cls, key):
-        handler = cls.r1p(key)
-        handler_version_id = handler.fetch1('version_id')
-        cls.Log('info', 'Running %s', handler.class_name)
-        cls.Log('debug', 'Running %s with key %s', handler.class_name, key)
-        assert  handler_version_id == cls.current_version_id, f'Version mismatch, event_handler version_id is {handler_version_id} but the current version_id is {cls.current_version_id}'
-        key = handler.run(key)
-        cls.Log('info', '%s ran successfully.', handler.class_name)
-        return key
+    @classproperty
+    def default_primary_attrs(cls):
+        return f"""
+        {cls.hash_name} : {event_handler_id_sqltype} # id of event handler
+        """
+
+    @classproperty
+    def extra_primary_attrs(cls):
+        return """"""
+
+    @classproperty
+    def default_secondary_attrs(cls):
+        return f"""
+        timestamp=CURRENT_TIMESTAMP : timestamp
+        """
+
+    @classproperty
+    def extra_secondary_attrs(cls):
+        return """"""
+
+
+class EventHandler(Base, djpb.BasePart, djpu.UserTable, dj.Part, dj.Lookup):
+    def __init_subclass__(cls, **kwargs):
+        cls._init_validation(**kwargs)
+
+    enable_hashing = True
+    hash_name = 'event_handler_id'
+    hashed_attrs = 'event', 'version'
+
+    @classproperty
+    def default_primary_attrs(cls):
+        return f"""
+        -> master
+        event : {event_sqltype}
+        """
+
+    @classproperty
+    def extra_primary_attrs(cls):
+        return """"""
+
+    @classproperty
+    def default_secondary_attrs(cls):
+        return f""""""
+
+    @classproperty
+    def extra_secondary_attrs(cls):
+        return """"""
+
+
+class Maker(Base, djpb.BasePart, djpu.UserTable, dj.Part, dj.Computed):
+    def __init_subclass__(cls, **kwargs):
+        cls._init_validation(**kwargs)
+    
+    enable_hashing = True
+    
+    @classproperty
+    def hash_name(cls):
+        return cls.hash_name
+    
+    @classproperty
+    def hashed_attrs(cls):
+        return cls.upstream.primary_key + cls.method.primary_key
+    
+    @classproperty
+    def default_primary_attrs(cls):
+        return f"""
+        -> master
+        -> {cls.upstream.class_name}
+        -> {cls.method.class_name}
+        """
+
+    @classproperty
+    def extra_primary_attrs(cls):
+        return """"""
+    
+    @classproperty
+    def default_secondary_attrs(cls):
+        return f""""""
+
+    @classproperty
+    def extra_secondary_attrs(cls):
+        return """"""
+
+    def make(self, key):
+        key[self.hash_name] = self.hash1(key)
+        key.update(self.upstream.get1(key))
+        key.update(self.method.run(key))
+        self.insert1(key, ignore_extra_fields=True, insert_to_master=True, insert_to_master_kws={'ignore_extra_fields': True, 'skip_duplicates': True}, skip_hashing=True)
+        self.on_make(key)
+
+    def on_make(self, key):
+        pass
